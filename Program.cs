171c8c5755cdd5371a177b558effe6dec0621df7
Salunke0497 <113031652+Salunke0497@users.noreply.github.com>
2022-12-17 02:10:13 +0530

    welcome

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicPograms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //vowel.VowelChar();
+            //swap.SwapNumber();
+            LargestNumber.max();
+            Console.ReadLine();
+        }
+    }
+}
