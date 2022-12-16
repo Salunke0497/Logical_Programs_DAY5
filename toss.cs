27e74e89f76ac383e4f085c38f04c9e773d0f824
Salunke0497 <113031652+Salunke0497@users.noreply.github.com>
2022-12-17 03:17:05 +0530

    welcome

---
+++ toss.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicPograms
+{
+    internal class toss
+    {
+        public static void main()
+        {
+            Console.WriteLine("Press ENTER for TOSS");
+            Console.ReadLine();
+            Random random = new Random();
+            int x = random.Next(2);
+            if(x == 0)
+            {
+                Console.WriteLine("head");
+            }else
+            {
+                Console.WriteLine("tail");
+            }
+        }
+    }
+}
