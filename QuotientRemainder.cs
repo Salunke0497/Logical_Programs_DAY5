ccd49412def6c6d405db971c9e9ecf6dcc0c3845
Salunke0497 <113031652+Salunke0497@users.noreply.github.com>
2022-12-18 13:51:57 +0530

    welcome

---
+++ QuotientRemainder.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicPograms
+{
+    internal class QuotientRemainder
+    {
+        public static void value()
+        {
+            int dividend = 210;
+            int divisior = 20;
+
+            int quotient = dividend / divisior;
+            int remainder = dividend % divisior;
+
+            Console.WriteLine("Dividend : {0} Divisior : {1}",dividend, divisior);
+            Console.WriteLine("Quotient = {0}", quotient);
+            Console.WriteLine("remainder = " + remainder);
+        }
+    }
+}
