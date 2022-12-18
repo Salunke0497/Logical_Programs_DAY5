b7857c2d035248d8704a6a47266996b41cdd6d77
Salunke0497 <113031652+Salunke0497@users.noreply.github.com>
2022-12-18 13:25:36 +0530

    welcome

---
+++ EvenOdd.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicPograms
+{
+    internal class EvenOdd
+    {
+        public static void evenodd()
+        {
+            Console.WriteLine("ENTER NUMBER HERE");
+            int X = Convert.ToInt32(Console.ReadLine());
+            if(X%2 == 0)
+            {
+                Console.WriteLine(X + " IS A EVEN NUMBER ");
+            }else
+            {
+                Console.WriteLine(X + " IS ODD NUMBER ");
+            }
+        }
+    }
+}
